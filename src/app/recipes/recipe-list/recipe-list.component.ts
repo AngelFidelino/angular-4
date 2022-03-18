a9bfad8502fa3ded7713873d493840bd712de125
angelflrod <angelflrod@gmail.com>
2022-03-18 11:07:11 -0600

    until section 5

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  constructor() { }
+  recipes : Recipe[] = [
+    new Recipe('Roast chicken','Chicken with chile-basil',
+    'https://static.onecms.io/wp-content/uploads/sites/9/2021/02/12/roast-chicken-with-chile-basil-vinaigrette-charred-broccoli-potatoes-FT-RECIPE0321.jpg')
+    ,
+    new Recipe('Roast chicken','Chicken with chile-basil',
+    'https://static.onecms.io/wp-content/uploads/sites/9/2021/02/12/roast-chicken-with-chile-basil-vinaigrette-charred-broccoli-potatoes-FT-RECIPE0321.jpg')
+  ];
+  constructor() { 
+    console.log('Lenght: '+this.recipes.length);
+  }
 
   ngOnInit(): void {
   }
